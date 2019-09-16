e69edfe0448f95606d151bb7afb59042aa88c5dc
Param Singh <iliekcomputers@gmail.com>
2019-09-15 19:38:11 -0700

    Merge run.sh and spark-submit.sh
    run.sh isn't really used so merging it is a good idea.
    Also, there's gonna be need for another script very soon
    sooooo.

--- spark-submit.sh
+++ spark-submit.sh
@@ -2,14 +2,27 @@
 
 source config.sh
 
+if [ "$#" -eq 0 ]; then
+    echo "Usage: spark-submit.sh <cmd_to_run> ..."
+    exit
+fi
+
 zip -r listenbrainz_spark.zip listenbrainz_spark/
-time ./run.sh /usr/local/spark/bin/spark-submit \
-	--packages org.apache.spark:spark-avro_2.11:2.4.1 \
-	--master $SPARK_URI \
-	--conf "spark.scheduler.listenerbus.eventqueue.capacity"=$LISTENERBUS_CAPACITY \
-    	--conf "spark.cores.max"=$MAX_CORES \
-    	--conf "spark.executor.cores"=$EXECUTOR_CORES \
-    	--conf "spark.executor.memory"=$EXECUTOR_MEMORY \
-    	--conf "spark.driver.memory"=$DRIVER_MEMORY \
-	--py-files listenbrainz_spark.zip \
-	"$@"
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+docker pull metabrainz/listenbrainz-spark:latest
+time docker run \
+    -v `pwd`:/rec \
+    --network spark-network \
+    --name $CONTAINER_NAME \
+    metabrainz/listenbrainz-spark:latest \
+		/usr/local/spark/bin/spark-submit \
+		--packages org.apache.spark:spark-avro_2.11:2.4.1 \
+		--master $SPARK_URI \
+		--conf "spark.scheduler.listenerbus.eventqueue.capacity"=$LISTENERBUS_CAPACITY \
+		--conf "spark.cores.max"=$MAX_CORES \
+		--conf "spark.executor.cores"=$EXECUTOR_CORES \
+		--conf "spark.executor.memory"=$EXECUTOR_MEMORY \
+		--conf "spark.driver.memory"=$DRIVER_MEMORY \
+		--py-files listenbrainz_spark.zip \
+		"$@"
