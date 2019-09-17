eff649831bf4615f85e91aad3c8ce9d491f000ed
Param Singh <iliekcomputers@gmail.com>
2019-09-16 19:36:23 -0700

    Python3 stuff

--- job_request_handler.py
+++ job_request_handler.py
@@ -2,10 +2,8 @@ import subprocess
 
 
 def spark_submit(options):
-    r = subprocess.call(['./py-spark-submit.sh', 'manage.py', 'user'], capture_output=True)
+    r = subprocess.run(['./py-spark-submit.sh', 'manage.py', 'user'], stdout=subprocess.PIPE)
     print(r.stdout)
-
-
 
 if __name__ == '__main__':
     spark_submit(None)
