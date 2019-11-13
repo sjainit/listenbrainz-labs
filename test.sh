d9a555e8e43d31ab700a684f4abbb4f2b5d5ec11
Param Singh <iliekcomputers@gmail.com>
2019-11-13 22:35:32 +0000

    rm existing containers

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+docker stop listenbrainz-spark-test-$USER
+docker rm listenbrainz-spark-test-$USER
 docker run \
     -v `pwd`:/rec \
     --name listenbrainz-spark-test \
     metabrainz/listenbrainz-spark:latest \
     py.test
+docker stop listenbrainz-spark-test-$USER
+docker rm listenbrainz-spark-test-$USER
