5e8a45471efabe835e03b080e9eb0e394418f9d1
Param Singh <iliekcomputers@gmail.com>
2019-11-13 21:01:16 +0000

    Create a test session

--- listenbrainz_spark/tests/__init__.py
+++ listenbrainz_spark/tests/__init__.py
@@ -5,4 +5,4 @@ import listenbrainz_spark
 class SparkTestCase(unittest.TestCase):
 
     def setUp(self):
-        listenbrainz_spark.init_spark_session('spark-test-run-{}'.format(str(uuid.uuid4())))
+        listenbrainz_spark.init_test_session('spark-test-run-{}'.format(str(uuid.uuid4())))
