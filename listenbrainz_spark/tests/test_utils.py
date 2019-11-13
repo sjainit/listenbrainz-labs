8decedc36783a15fc3b199bcc90b06748758a4d5
Param Singh <iliekcomputers@gmail.com>
2019-11-13 23:28:15 +0000

    bugfix

--- listenbrainz_spark/tests/test_utils.py
+++ listenbrainz_spark/tests/test_utils.py
@@ -6,7 +6,7 @@ from listenbrainz_spark.tests import SparkTestCase
 class UtilsTestCase(SparkTestCase):
 
     def test_create_dataframe(self):
-        df = utils.create_dataframe({'column1': 1, 'column2': 2})
+        df = utils.create_dataframe({'column1': 1, 'column2': 2}, schema=None)
         self.assertEqual(df.count(), 1)
         path = 'hdfs://hadoop-master:9000/test_df.parquet'
         utils.save_parquet(df, path)
