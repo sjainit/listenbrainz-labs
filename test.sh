9035c29284779cd2ed23d315f2b22856ff9a3052
Param Singh <iliekcomputers@gmail.com>
2019-11-13 22:37:00 +0000

    fix container

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ docker stop listenbrainz-spark-test-$USER
 docker rm listenbrainz-spark-test-$USER
 docker run \
     -v `pwd`:/rec \
-    --name listenbrainz-spark-test \
+    --name listenbrainz-spark-test-$USER \
     metabrainz/listenbrainz-spark:latest \
     py.test
 docker stop listenbrainz-spark-test-$USER
