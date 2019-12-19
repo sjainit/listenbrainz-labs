b062aa5c2e9711291bcb71e31236ce61268dc756
sjainit <sarthak2907@gmail.com>
2019-12-19 14:44:28 +0530

    Added tests for utils.py

--- listenbrainz_spark/stats/user/tests/test_utils.py
+++ listenbrainz_spark/stats/user/tests/test_utils.py
@@ -69,4 +69,34 @@ class UtilsTestCase(SparkTestCase):
         self.assertEqual(u1.get_recordings('table'), dictionary)
         self.assertGreater(u1.get_recordings('table')['user1'][0]['listen_count'], u1.get_recordings('table')['user2'][0]['listen_count'])
         self.assertEqual(list(u1.get_recordings('table'))[0], 'user1')
-        
+    
+    def test_get_releases(self):
+        df = utils.create_dataframe(Row(user_name='user2',artist_name='artist1', artist_msid='1',artist_mbids='1',release_name='test', release_msid='1', release_mbid='1'), schema=None)
+        df1 = utils.create_dataframe(Row(user_name='user1',artist_name='artist1', artist_msid='1',artist_mbids='1',release_name='test', release_msid='1', release_mbid='1'), schema=None)
+        df = df.union(df1)
+        df2 = utils.create_dataframe(Row(user_name='user1',artist_name='artist1', artist_msid='1',artist_mbids='1',release_name='test', release_msid='1', release_mbid='1'), schema=None)
+        df = df.union(df2)
+        df.createOrReplaceTempView("table")
+        dictionary =  {
+                    'user1' : [{
+                        'artist_name': 'artist1',
+                        'artist_msid': '1',
+                        'artist_mbids': '1',
+                        'release_name': 'test',
+                        'release_msid': '1',
+                        'release_mbid': '1',
+                        'listen_count': 2
+                    }],
+                    'user2' : [{
+                        'artist_name': 'artist1',
+                        'artist_msid': '1',
+                        'artist_mbids': '1',
+                        'release_name': 'test',
+                        'release_msid': '1',
+                        'release_mbid': '1',
+                        'listen_count': 1
+                    }]
+                }
+        self.assertEqual(u1.get_releases('table'), dictionary)
+        self.assertGreater(u1.get_releases('table')['user1'][0]['listen_count'], u1.get_releases('table')['user2'][0]['listen_count'])
+        self.assertEqual(list(u1.get_releases('table'))[0], 'user1')
